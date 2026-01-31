f669080ac480c09deb11517034c6dd0f19598423
Misamu12 <patrickmisamu77@gmail.com>
2026-02-01 00:30:07 +0100

    let go

---
+++ app/+not-found.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function notfound() {
+  
+  <View>
+    <Text style={styles.bigT} >404</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  bigT : {
+    color : 'red'
+  }
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,27 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  const isLogin = true;
+
   return (
     <Stack>
       <Stack.Screen
         name="HomeScreen"
-        options={{ title: "Accueil", headerShown: false }}
+        options={{ title: "Acceuil", headerShown: true }}
+      />
+
+      <Stack.Screen
+        name="connexion"
+        options={{ title: "Login", headerShown: false }}
+      />
+      <Stack.Screen
+        name="historique"
+        options={{ title: "historique", headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="+not-found"
+        options={{ title: "404", headerShown: false }}
       />
     </Stack>
   );
